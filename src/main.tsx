430592dffc8a88bf34dffd2a8677f93eb66f8d92
burak behlül <burakyabgu@gmail.com>
2025-07-09 12:53:10 +0300

    feat: Add routers

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!, {
+  onRecoverableError(error, errorInfo) {
+    console.error('An error occurred: ', error, errorInfo);
+  }
+}).render(
   <StrictMode>
     <App />
   </StrictMode>,
---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { useRoutes } from "react-router-dom";
+
+export default function Routes() {
+    return useRoutes([
+        {
+            path: "/",
+            element: <div>Welcome to the Home Page</div>
+        }
+    ])
+}
